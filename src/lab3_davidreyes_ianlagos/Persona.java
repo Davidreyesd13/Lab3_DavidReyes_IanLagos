9a7f908d0a75495870179c80fc433368c6391773
ianlagos <ianlagos@hotmail.com>
2020-02-07 14:00:00 -0600

    clase Persona

---
+++ src/lab3_davidreyes_ianlagos/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_davidreyes_ianlagos;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String Años_profecionales;
+    private double salario;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String Años_profecionales, double salario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.Años_profecionales = Años_profecionales;
+        this.salario = salario;
+    }
+    
+}
