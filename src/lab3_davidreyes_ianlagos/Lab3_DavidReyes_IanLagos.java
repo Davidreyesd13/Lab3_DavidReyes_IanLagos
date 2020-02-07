7dbb8db75ec0c459159f1c38db3581600b7959c0
David Reyes <davidreyesd13@gmail.com>
2020-02-07 13:35:54 -0600

    Inicio de lab

---
+++ src/lab3_davidreyes_ianlagos/Lab3_DavidReyes_IanLagos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_davidreyes_ianlagos;
+
+/**
+ *
+ * @author david
+ */
+public class Lab3_DavidReyes_IanLagos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
